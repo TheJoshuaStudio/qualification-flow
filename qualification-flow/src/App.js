8f3a109dfe609fd8cbb9ab5a9ff304b568c48768
Danielle <danibroadwater@outlook.com>
2021-07-17 22:06:29 -0500

    added components,start design, json data

--- qualification-flow/src/App.js
+++ qualification-flow/src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import { Switch, Route  } from "react-router-dom";
+import QuestionsCard from './components/MainContainer/QuestionsCard';
+import questionsData from './data/questions.json'
+import InputFields from './components/MainContainer/inputFields';
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      {step === 1 && <QuestionsCard
+        data={questionsData.data[activeQuestion]}
+        onAnswerUpdate={setAnswers}
+        numberOfQuestions={questionsData.data.length}
+        activeQuestion={activeQuestion}
+        onSetActiveQuestion={setActiveQuestion}
+        onSetStep={setStep}
+      />}
+      {step === 2 && <InputFields/>}
     </>
   );
 }
---
+++ qualification-flow/src/components/MainContainer/QuestionsCard.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef} from 'react';
+import '../MainContainer/questionCard.css';
+import 'bulma/css/bulma.min.css';
+// import NavBar from '../NavBar/NavBar';
+// import inputFields from './inputFields';
+
+export default function QuestionsCard({ data, onAnswerUpdate, numberOfQuestions, activeQuestion, onSetActiveQuestion,onSetStep }) {
+  // const [step, setStep] = useState(1);
+  const [selected, setSelected] = useState('');
+  const [error, setError] = useState('');
+  const radiosWrapper = useRef();
+
+  useEffect(() => {
+    const findCheckedInput = radiosWrapper.current.querySelector('input:checked');
+    if(findCheckedInput) {
+      findCheckedInput.checked = false;
+    }
+  }, [data]);
+
+  const changeHandler = (e) => {
+    setSelected(e.target.value);
+    if(error) {
+      setError('');
+    }
+  }
+
+  const nextClickHandler = (e) => {
+    if(selected === '') {
+      return setError('Please select one option!');
+    }
+    onAnswerUpdate(prevState => [...prevState, { q: data.question, a: selected }]);
+    setSelected('');
+    if (activeQuestion < numberOfQuestions - 1) {
+      onSetActiveQuestion(activeQuestion + 1);
+    } else {
+      onSetStep(2);
+    }
+  }
+
+  return (
+    <div className="mainContainer">
+      <div className="card">
+       <div className="card-content">
+           <div className="content">
+            <h2 className="mb-5">{data.question}</h2>
+            <div className="control" ref={radiosWrapper}>
+              {data.choices.map((choice, i) => (
+                <label className="radio has-background-light" key={i}>
+                  <input type="radio" name="answer" value={choice} onChange={changeHandler} />
+                  {choice}
+                </label>
+              ))}
+            </div>
+            {error && <div className="has-text-danger">{error}</div>}
+            <button className="button is-link is-medium is-fullwidth mt-4" onClick={nextClickHandler}>Next</button>
+          </div>
+         
+         </div>
+      </div>
+   </div>
+  );
+}
