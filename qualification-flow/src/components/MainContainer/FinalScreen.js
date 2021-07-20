ddae8b8594ae920798b3f12a93fd33990ec57934
Danielle <danibroadwater@outlook.com>
2021-07-19 23:14:09 -0500

    final screen style started

--- qualification-flow/src/components/MainContainer/FinalScreen.js
+++ qualification-flow/src/components/MainContainer/FinalScreen.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function FinalScreen(props) {
   return (
-    <div>
-      <h1>CONGRATS, you may be a good fit to save up to 60%* on your healthcare costs!
-</h1>
-    </div>
+    <div className="hero is-fullheight">
+      <div className="card ">
+       <div className="card-content">
+           <div className="content">
+            <h2 className="mb-5">CONGRATS, you may be a good fit to save up to 60%* on your healthcare costs!</h2>
+          </div>
+        </div>
+      </div>
+      </div>
+   
   );
 }
 
